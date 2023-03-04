3084484d9049e0bea2ccd05b45b1cddad1ffb051
Vijesh Kumar <118898134+itsvijeshkumar@users.noreply.github.com>
2023-03-04 17:08:28 +0400

    Add files via upload

---
+++ src/main/java/com/microservice/LimitServices/LimitsConfigurationController.java
@@ -0,0 +1,20 @@
+package com.microservice.LimitServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javatpoint.microservices.limitservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limit")
+	public LimitConfiguration retriveLimitsForm() {
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());  
+	}
+}
